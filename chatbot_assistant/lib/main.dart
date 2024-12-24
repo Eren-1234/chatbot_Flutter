e6801a31b509788ae087751634a17231ad5c6198
Eren Tarık Kınacı <99985487+Eren-1234@users.noreply.github.com>
2024-12-24 20:45:12 +0300

    Add files via upload

---
+++ chatbot_assistant/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'helper/global.dart';
+import 'screen/splash_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: appName,
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData(
+          useMaterial3: false,
+          appBarTheme: const AppBarTheme(
+            elevation: 1,
+            centerTitle: true,
+            backgroundColor: Colors.white,
+            iconTheme: IconThemeData(color: Colors.blue),
+            titleTextStyle: TextStyle(
+                color: Colors.blue, fontSize: 20, fontWeight: FontWeight.w500),
+          )),
+
+      //
+      home: const SplashScreen(),
+    );
+  }
+}
+
+
